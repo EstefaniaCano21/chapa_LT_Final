af65c8cbd363e794d51963a520a3d48acd41fd2f
EstefaniaCano21 <acanor5@miumg.edu.gt>
2025-06-02 18:30:58 -0600

    Update and rename alu_8bit.v to mealy.v

---
+++ src/mealy.v
@@ -0,0 +1,25 @@
+module fsm_mealy (
+    input [3:0] digit,
+    input [1:0] state,
+    input enter,
+    output reg correct_digit
+);
+
+    wire [3:0] pin0 = 4'd9;
+    wire [3:0] pin1 = 4'd9;
+    wire [3:0] pin2 = 4'd7;
+    wire [3:0] pin3 = 4'd9;
+
+    wire [3:0] pin_selected;
+
+    assign pin_selected = (state == 2'd0) ? pin0 :
+                          (state == 2'd1) ? pin1 :
+                          (state == 2'd2) ? pin2 :
+                                            pin3;
+
+    always @(*) begin
+        correct_digit = (enter && (digit == pin_selected));
+    end
+
+endmodule
+
